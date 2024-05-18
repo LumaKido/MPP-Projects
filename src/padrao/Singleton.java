37c46dbeb5913feec1a155a6709bfd45519e62ca
LumaKido <lumamari499@gmail.com>
2024-05-17 21:51:26 -0300

    First commit

---
+++ src/padrao/Singleton.java
@@ -0,0 +1,22 @@
+package padrao;
+
+public class Singleton {
+	private static volatile Singleton instance;
+	private String data;
+	
+	private Singleton(String data) {
+		this.data = data;
+	}
+	
+	public static Singleton getInstance(String data) {
+		Singleton result = instance;
+		if(result == null) {
+			synchronized (Singleton.class){
+				if(result == null) {
+					instance = result = new Singleton(data);
+				}
+			}
+		}	
+		return result;
+	}
+}
